ef6361cb5326d3822ab231a0bed0c6cd220c3100
@StevenBLR <s.belair95@gmail.com>
2021-04-04 18:33:27 +0200

    Quit form feature

--- project/js/starterOnly/modal.js
+++ project/js/starterOnly/modal.js
@@ -10,14 +10,19 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const quiBt = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+quiBt.addEventListener("click", quitModal);
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+function quitModal(){
+  modalbg.style.display = "none";
 }
 
 
